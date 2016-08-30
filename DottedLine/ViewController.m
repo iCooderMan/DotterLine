1224f2fd3411ef17eed63e2234cb7fbf62b1efc9
Ali <604999439@qq.com>
2016-08-30 13:13:44 +0800

    first

--- DottedLine/ViewController.m
+++ DottedLine/ViewController.m
@@ -2,11 +2,12 @@
 //  ViewController.m
 //  DottedLine
 //
-//  Created by Almas on 16/8/29.
+//  Created by Ali on 16/8/29.
 //  Copyright © 2016年 Ali. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "UIView+DotterLine.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+/** 横向view 虚线 **/
+    UIView *dotterLineTransverse = [[UIView alloc]initWithFrame:CGRectMake(40, 40, 200, 2)];
+    [dotterLineTransverse drawTransverseDotterLineWithLength:6 lineSpacing:2 lineColor:[UIColor redColor]];
+    [self.view addSubview:dotterLineTransverse];
+    
+    
+    /** 竖向View 虚线 **/
+    UIView *dotterLineVertical = [[UIView alloc]initWithFrame:CGRectMake(40, 40, 2, 200)];
+    [dotterLineVertical drawVerticalDotterLineWithHeight:6 lineSpacing:2 lineColor:[UIColor redColor]];
+    [self.view addSubview:dotterLineVertical];
+    
+    
+    /** 给当前view 添加虚线边框 **/
+    UIView *dotterLineRect= [[UIView alloc]initWithFrame:CGRectMake(40, 260, 200, 200)];
+    dotterLineRect.backgroundColor = [UIColor yellowColor];
+    [dotterLineRect drawRectDotterLineWithLineWight:4 lineLength:4 lineSpacing:2 lineColor:[UIColor redColor] cornerRadius:25];
+    
+    [self.view addSubview:dotterLineRect];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
